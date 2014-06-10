5ba458d8d6b13650cf5cd4402f018b43a8733a5b
CocoZohan <dos93@mail.ru>
2014-06-10 11:34:39 +0600

    custom list (rough)

--- src/com/example/myapp/MyActivity.java
+++ src/com/example/myapp/MyActivity.java
@@ -2,53 +2,25 @@ package com.example.myapp;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class MyActivity extends Activity implements View.OnClickListener {
+public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
-
-    Button b1, b2, b3, b4;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Buttons connect to view-elements
-        b1 = (Button)findViewById(R.id.button);
-        b2 = (Button)findViewById(R.id.button2);
-        b3 = (Button)findViewById(R.id.button3);
-        b4 = (Button)findViewById(R.id.button4);
-
-        // Action listener is set
-        b1.setOnClickListener(this);
-        b2.setOnClickListener(this);
-        b3.setOnClickListener(this);
-        b4.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        // Actions performed when one of the buttons is pressed
-        // Toasts with the name of pressed button appear
-        switch(v.getId()){
-            case R.id.button:
-                Toast.makeText(this, "Button 1 is pressed", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button2:
-                Toast.makeText(this, "Button 2 is pressed", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button3:
-                Toast.makeText(this, "Button 3 is pressed", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button4:
-                Toast.makeText(this, "Button 4 is pressed", Toast.LENGTH_SHORT).show();
-                break;
-        }
+        Intent intent = new Intent(this, List.class);
+        startActivity(intent);
     }
 }
